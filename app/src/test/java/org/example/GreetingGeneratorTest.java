c8d8600db3401848f37efc7a58fc6a9f02b39a5d
Juunibyou <danijunn@gmail.com>
2025-08-27 17:22:58 -0500

    update

--- app/src/test/java/org/example/GreetingGeneratorTest.java
+++ app/src/test/java/org/example/GreetingGeneratorTest.java
@@ -1,15 +1,15 @@
-package org.example;
+// package org.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.*;
 
-class GreetingGeneratorTest {
-  // This is just an example test file to demonstrate how everything works
-  // You should delete this file when you implement your own solution
+// class GreetingGeneratorTest {
+//   // This is just an example test file to demonstrate how everything works
+//   // You should delete this file when you implement your own solution
 
-  @Test
-  void itSaysHello() {
-    Power generator = new Power();
-    assertEquals(generator.sayHello(), "Hello World!");
-  }
-}
+//   @Test
+//   void itSaysHello() {
+//     Power generator = new Power();
+//     assertEquals(generator.sayHello(), "Hello World!");
+//   }
+// }
